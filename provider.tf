d04b8b28275b4f5c260205e92099f9bf89598a96
Avi Han <Avi.Han@siemens.com>
2023-04-20 15:00:00 +0300

    Configure TF backend

--- provider.tf
+++ provider.tf
@@ -1,37 +1,13 @@
 provider "aws" {
-  region  = "ap-south-1" # Don't change the region
+  region = "ap-south-1" # Don't change the region
 }
 
 # Add your S3 backend configuration here
 
 terraform {
   backend "s3" {
-    #key = "terraform.tfstate"
-    key = "Avi.Han"
-
+    bucket = "3.devops.candidate.exam"
+    key    = "Avi.Han"
+    region = "ap-south-1"
   }
 }
-
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      # version = "~> 4.50"
-    }
-  }
-}
-
-resource "aws_s3_bucket" "terraform_s3_devops_exam" {
-  bucket = "3.devops.candidate.exam"
-  provider = "aws"
-
-  server_side_encryption_configuration {
-    rule {
-      bucket_key_enabled = true
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
-
-}
-
