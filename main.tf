aa60787b4f6aa20e9943a031348a8328699c9de9
Avi Han <Avi.Han@siemens.com>
2023-04-20 16:39:02 +0300

    tag vpc

--- main.tf
+++ main.tf
@@ -4,8 +4,10 @@ data "aws_availability_zones" "available" {}
 
 resource "aws_vpc" "vpc" {
 
-#   id         = data.aws_vpc.vpc.id
   cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "Avi_VPC"
+  }
 
 }
 resource "aws_eip" "terraformtraining-nat" {
