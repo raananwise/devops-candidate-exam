044db013eca5be71f393da31e3c859bd2e37f216
Avi Han <Avi.Han@siemens.com>
2023-04-20 14:02:14 +0300

    provider + s3 config

--- provider.tf
+++ provider.tf
@@ -3,3 +3,35 @@ provider "aws" {
 }
 
 # Add your S3 backend configuration here
+
+terraform {
+  backend "s3" {
+    #key = "terraform.tfstate"
+    key = "Avi.Han"
+
+  }
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      # version = "~> 4.50"
+    }
+  }
+}
+
+resource "aws_s3_bucket" "terraform_s3_devops_exam" {
+  bucket = "3.devops.candidate.exam"
+  provider = "aws"
+
+  server_side_encryption_configuration {
+    rule {
+      bucket_key_enabled = true
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+
+}
+
